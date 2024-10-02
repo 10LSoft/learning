e18aa9445b2571094253d1df015e203b551633a4
Genese Lessa <genese.lessa@gmail.com>
2024-10-02 20:30:54 -0300

    fixing singleton

--- computer-science/adicionais/design-patterns/python/criacionais/singleton.py
+++ computer-science/adicionais/design-patterns/python/criacionais/singleton.py
@@ -10,7 +10,6 @@ class Singleton:
 
     def __new__(cls):
         if not hasattr(cls, "instance"):
-            # cls.instance = super(Singleton, cls).__new__(cls)
             cls.instance = super().__new__(cls)
 
         return cls.instance
@@ -21,36 +20,3 @@ s2 = Singleton()
 
 print(s1 == s2)  # True
 print(id(s1), id(s2))  # same memory address
-
-
-class Singleton2:
-    """
-    Another reason to create a singleton is when we need to make some lazy
-    instantiation. It means that when we don't need to instantiate an object,
-    we can only call __init__ for example and only by another method create the
-    true object itself. Works like this:
-    """
-
-    _instance = None
-
-    def __init__(self):
-        if not Singleton2._instance:
-            print("this object was not created yet")
-        else:
-            return self.create()
-
-    @classmethod
-    def create(cls):
-        if not cls._instance:
-            cls._instance = Singleton2()
-
-        return cls._instance
-
-
-Singleton2()
-
-s3 = Singleton2()
-s4 = Singleton2()
-
-print(s3 == s4)
-print(id(s3), id(s4))
