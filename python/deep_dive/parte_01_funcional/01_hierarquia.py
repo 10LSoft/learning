c85984f1f9d14b94de0628774f7f4fa4d3b51452
Genese Lessa <genese.lessa@gmail.com>
2025-06-03 03:24:12 -0300

    some other things

---
+++ python/deep_dive/parte_01_funcional/01_hierarquia.py
@@ -0,0 +1,11 @@
+"""
+Python possui alguns tipos hierárquicos de dados, como listas, tuplas e
+dicionários, mas há ainda tipos mais fundamentais.
+
+Podemos por exemplo citar o tipo 'number', que é base para outros tipos mais
+complexos. Dentro os tipos numéricas podemos fazer inicialmente uma separação
+classificativa entro números integrais e números não-integrais.
+
+Integrais: inteiros, booleanos, complexos.
+Não-integrais: float, decimal, frações.
+"""
